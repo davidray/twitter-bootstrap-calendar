925ff5ae65bf84f07f07761a6e1fbc7ebcbf75ed
David Christiansen <dave@developertown.com>
2013-01-11 09:06:09 -0600

    Turbolinks compatibility ftw

--- vendor/assets/javascripts/twitter-bootstrap-calendar/index.js
+++ vendor/assets/javascripts/twitter-bootstrap-calendar/index.js
@@ -1,11 +1,19 @@
+function tbsCalendarSetMaxHeight(){
+  var maxHeight = -1;
+
+  $('.calendar_grid .week .span1').each(function() {
+    maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+  });
+
+  $('.calendar_grid .week .span1').each(function() {
+    $(this).height(maxHeight);
+  });
+}
+
+$(document).on('page:load', function() {
+  tbsCalendarSetMaxHeight();
+});
+
 $(document).ready(function() {
-   var maxHeight = -1;
-
-   $('.calendar_grid .week .span1').each(function() {
-     maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
-   });
-
-   $('.calendar_grid .week .span1').each(function() {
-     $(this).height(maxHeight);
-   });
- });
+  tbsCalendarSetMaxHeight();
+});
